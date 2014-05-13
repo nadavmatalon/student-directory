aa0e529cc057ee5a7d7f6cd2da9109bc4cf2bcbe
Nadav <nadav.matalon@gmail.com>
2014-05-13 13:36:24 +0100

    added array for student names

--- directory.rb
+++ directory.rb
@@ -1,28 +1,34 @@
+#puts all the students in an arry
+students = [
+	"Roi Driscoll",
+	"James Whyte",
+	"Margherita Serino",
+	"Vaidas Mykolaitis",
+	"Johann Bornman",
+	"Kate Hamilton",
+	"James Keap",
+	"Nic Yeeles",
+	"Julie Walker",
+	"Will Allen",
+	"Julia Tan",
+	"Federico Maffei",
+	"Jamie Patel",
+	"Faezrah Rizalman",
+	"Josh Fail-Brown",
+	"Sasha Cooper",
+	"Nicolai DTH",
+	"Nadav Matalon",
+	"Fitsum Teklehaimanot"	
+]
 
-student_count = 19   #number of students in May 2014 cohort
-
-#first we print the list of students
+#prints the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
-puts "Roi Driscoll"
-puts "James Whyte"
-puts "Margherita Serino"
-puts "Vaidas Mykolaitis"
-puts "Johann Bornman"
-puts "Kate Hamilton"
-puts "James Keap"
-puts "Nic Yeeles"
-puts "Julie Walker"
-puts "Will Allen"
-puts "Julia Tan"
-puts "Federico Maffei"
-puts "Jamie Patel"
-puts "Faezrah Rizalman"
-puts "Josh Fail-Brown"
-puts "Sasha Cooper"
-puts "Nicolai DTH"
-puts "Nadav Matalon"
-puts "Fitsum Teklehaimanot"
+
+students.each do |student|
+	puts student
+end
+
 
 #finally. we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
